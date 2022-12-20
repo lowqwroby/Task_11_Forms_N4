911d66c85cd2dbfac2b86a4af81b2f8d6bdc0184
lowqwroby <eltiobvs@gmail.com>
2022-12-20 20:49:43 +0300

    Добавьте файлы проекта.

---
+++ Task_11_Forms_N4/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Task_11_Forms_N4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(97, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Введите дату";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(15, 28);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(122, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 53);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(101, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Прошлый день";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(140, 53);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(99, 16);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Будущий день";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(15, 72);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(122, 22);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(143, 72);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(122, 22);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 97);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(179, 16);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "До конца месяца осталось";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(224, 97);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(42, 16);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "дней.";
+			// 
+			// textBox4
+			// 
+			this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox4.Location = new System.Drawing.Point(206, 97);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ReadOnly = true;
+			this.textBox4.Size = new System.Drawing.Size(21, 15);
+			this.textBox4.TabIndex = 8;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(12, 118);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(64, 16);
+			this.label6.TabIndex = 9;
+			this.label6.Text = "Этот год";
+			// 
+			// textBox5
+			// 
+			this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox5.Location = new System.Drawing.Point(82, 118);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.ReadOnly = true;
+			this.textBox5.Size = new System.Drawing.Size(183, 15);
+			this.textBox5.TabIndex = 10;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(143, 139);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(122, 23);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Определить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(297, 181);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Работа с классами";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Task_11_Forms_N4/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_11_Forms_N4
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if(textBox1.Text == "")
+				{
+					DataWork data = new DataWork();
+					textBox1.Text = string.Format(data.DateValue);
+					textBox2.Text = string.Format(data.LastDay(data.date));
+					textBox3.Text = string.Format(data.NextDay(data.date));
+					textBox4.Text = Convert.ToString(data.DaysLeft(data.date));
+					if (data.LeapYear)
+					{
+						textBox5.Text = string.Format("високосный.");
+					}
+					else
+					{
+						textBox5.Text = string.Format("не високосный.");
+					}
+				}
+				else
+				{
+					DateTime setDate = DateTime.Parse(textBox1.Text);
+					DataWork data1 = new DataWork(setDate);
+					textBox1.Text = string.Format(data1.DateValue);
+					textBox2.Text = string.Format(data1.LastDay(data1.date));
+					textBox3.Text = string.Format(data1.NextDay(data1.date));
+					textBox4.Text = Convert.ToString(data1.DaysLeft(data1.date));
+					if (data1.LeapYear)
+					{
+						textBox5.Text = string.Format("високосный.");
+					}
+					else
+					{
+						textBox5.Text = string.Format("не високосный.");
+					}
+				}
+			}
+			catch (FormatException)
+			{
+				MessageBox.Show("Неверный ввод данных", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+			catch
+			{
+				MessageBox.Show("Неизвестная ошибка", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+	class DataWork
+	{
+		public DateTime date;
+
+		public DataWork()
+		{
+			date = new DateTime(2009, 1, 1);
+		}
+
+		public DataWork(DateTime date)
+		{
+			this.date = date;
+		}
+
+		public string LastDay(DateTime date)
+		{
+			DateTime ldate = date.AddDays(-1);
+			return ldate.ToShortDateString();
+		}
+
+		public string NextDay(DateTime date)
+		{
+			DateTime ndate = date.AddDays(1);
+			return ndate.ToShortDateString();
+		}
+
+		public int DaysLeft(DateTime date)
+		{
+			int dLeft = DateTime.DaysInMonth(date.Year, date.Month) - date.Day;
+			return dLeft;
+		}
+
+		public string DateValue
+		{
+			get
+			{
+				return date.ToShortDateString();
+			}
+			set
+			{
+				date = DateTime.Parse(value);
+			}
+		}
+
+		public bool LeapYear
+		{
+			get
+			{
+				if (date.Year % 4 == 0)
+					return true;
+				else
+					return false;
+			}
+		}
+	}
+}
